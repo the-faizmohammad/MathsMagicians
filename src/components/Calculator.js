0271698fbc9de955350d88aa49ab0aaec878bb8d
user-faiz <email2faizm@gmail.com>
2023-08-24 03:06:02 +0530

    Create Calculator function

---
+++ src/components/Calculator.js
@@ -0,0 +1,8 @@
+const Calculator = () => (
+    <div className="container">
+      <div className="input">
+        <input type="text" placeholder="0" id="display" required className="field" />
+      </div>
+      <Btn />
+    </div>
+  );
