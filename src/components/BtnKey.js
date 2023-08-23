53b9638e88abcb8023ab27dd740d4d6eaf45fc79
user-faiz <email2faizm@gmail.com>
2023-08-24 05:08:08 +0530

    Adding event listener to tiles btn

--- src/components/BtnKey.js
+++ src/components/BtnKey.js
@@ -2,7 +2,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
+const Btn = ({ onClick }) => (
+  <div className="buttons">
+    <button className="tiles btn" type="button" onClick={() => onClick('AC')}>AC</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('+/-')}>+/-</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('%')}>%</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('7')}>7</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('8')}>8</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('9')}>9</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('4')}>4</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('5')}>5</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('6')}>6</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('1')}>1</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('2')}>2</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('3')}>3</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('0')}>0</button>
+    <button className="tiles btn" type="button" onClick={() => onClick('.')}>.</button>
+  </div>
+);
 
 // Add prop type validation
 Btn.propTypes = {
