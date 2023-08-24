52af09d524078501b7ee1fd3c6af381d5798785b
user-faiz <email2faizm@gmail.com>
2023-08-24 15:00:17 +0530

    Add Quote function

---
+++ src/Api/Quote.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+const Quote = () => {
+  const [quote, setQuote] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+};
+
+export default Quote;
