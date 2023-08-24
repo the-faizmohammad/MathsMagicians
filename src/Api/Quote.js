db2ecd9d289764275e1a9c22a5adf3f351f6d141
user-faiz <email2faizm@gmail.com>
2023-08-24 15:01:50 +0530

    Add Use effect , api url and api key

--- src/Api/Quote.js
+++ src/Api/Quote.js
@@ -5,6 +5,37 @@ const Quote = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
+  useEffect(() => {
+    fetch('https://api.api-ninjas.com/v1/quotes?category=maths', {
+      headers: {
+        'X-Api-Key': 'B8Aei7hWtexMgroc3DcWRw==lt7kKkbwNfnSGwHo'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setQuote(data[0].quote);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError('An error occurred while fetching the quote.');
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div>
+      <h2>Math Quote</h2>
+      <p>{quote}</p>
+    </div>
+  );
 };
 
 export default Quote;
