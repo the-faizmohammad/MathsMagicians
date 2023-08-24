50c0bbcf82664431816070b3124b3949b4b038cb
user-faiz <email2faizm@gmail.com>
2023-08-24 17:27:08 +0530

    fixed linters error

--- src/Api/Quote.js
+++ src/Api/Quote.js
@@ -1,39 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import './Quote.css';
+
+const quoteCategory = 'funny';
+const quotesApiBaseUrl = 'https://api.api-ninjas.com/v1/quotes?category=';
+const apiUrl = `${quotesApiBaseUrl}${quoteCategory}`;
 
 const Quote = () => {
-  const [quote, setQuote] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
+  const [quoteData, setQuoteData] = useState({});
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://api.api-ninjas.com/v1/quotes?category=maths', {
-      headers: {
-        'X-Api-Key': 'B8Aei7hWtexMgroc3DcWRw==lt7kKkbwNfnSGwHo',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setQuote(data[0].quote);
-        setLoading(false);
-      })
-      .catch(() => {
-        setError('An error occurred while fetching the quote.');
-        setLoading(false);
-      });
+    let isCancelled = false;
+
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(apiUrl, {
+          headers: {
+            'X-Api-Key': 'uCHBJSjC0mhJo8VJsKMGKw==TE5WVQkHClEOg2cP',
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+
+        if (!isCancelled && Array.isArray(data) && data.length > 0) {
+          setQuoteData(data[0]);
+        }
+      } catch (error) {
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isCancelled = true;
+    };
   }, []);
 
-  if (loading) {
-    return <p>Loading...</p>;
+  if (isLoading) {
+    return <div>Loading quote</div>;
   }
 
-  if (error) {
-    return <p>{error}</p>;
+  if (hasError) {
+    return <div>Something went wrong!</div>;
   }
 
   return (
-    <div>
-      <h2>Math Quote</h2>
-      <p>{quote}</p>
+    <div className="quote-display">
+      <p className="quote">{quoteData.quote}</p>
+      <p className="author">
+        <b>{quoteData.author}</b>
+      </p>
     </div>
   );
 };
