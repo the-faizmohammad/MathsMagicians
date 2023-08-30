3f91170f3296a8d920134f8676d71e5699d435f7
user-faiz <email2faizm@gmail.com>
2023-08-30 07:15:22 +0530

    Add content to homepage

---
+++ src/spa/home.js
@@ -0,0 +1,40 @@
+import '../spa/css';
+
+const Home = () => (
+  <div className="container">
+    <h1 className="heading">Welcome to Math Magicians! There is a Calculator Next door😁</h1>
+    <div className="inner-container">
+      <h2>The History of the Calculator: From Abacus to Digital Marvels</h2>
+        <div className="paragraph">
+        <article>
+    <h3>The Evolution of Calculators: From Abacus to Digital Marvels</h3>
+    <p>Calculators have a rich history that spans centuries, from ancient abacuses to today's powerful digital devices. Let's explore the fascinating journey of how calculators have transformed over time.</p>
+
+    <h3>Ancient Origins: The Abacus</h3>
+    <p>The earliest form of a calculator was the abacus, used by civilizations such as the Mesopotamians, Egyptians, and Chinese. This simple device consisted of beads on rods, allowing users to perform basic arithmetic calculations. The abacus provided a solid foundation for numerical understanding and computation.</p>
+
+    <h3>Mechanical Calculators: Renaissance to 19th Century</h3>
+    <p>In the Renaissance, significant advancements in calculator technology were made. Blaise Pascal's "Pascaline," invented in the 17th century, was a mechanical calculator that could perform addition and subtraction. This marked the beginning of mechanical calculators.</p>
+    <p>In the 19th century, Charles Babbage's designs for the "Difference Engine" and "Analytical Engine" laid the groundwork for modern computing concepts. These machines used gears, levers, and cranks to perform calculations.</p>
+
+    <h3>Electronic Revolution: 20th Century</h3>
+    <p>The 20th century saw the rise of electronic calculators. The introduction of vacuum tubes and transistors revolutionized computation. In the 1960s, the "ANITA Mk VII" and "Cal Tech" were among the first successful electronic calculators, though they were large and costly.</p>
+
+    <h3>Interesting Facts</h3>
+    <ul>
+      <li>The "Cal Tech" calculator, introduced in the 1960s, weighed around 3.5 pounds and cost $400.</li>
+      <li>Casio's "Casiotron" from 1974 combined a calculator and digital watch.</li>
+      <li>HP's "HP-35" (1972) was the first pocket scientific calculator, capable of trigonometric functions.</li>
+    </ul>
+
+    <h3>Modern Uses and Integration</h3>
+    <p>Today, calculators are an essential part of daily life. They've evolved into compact digital devices, often integrated into computers and smartphones. From basic arithmetic to complex scientific calculations, calculators remain indispensable in education, finance, engineering, and more.</p>
+
+    <p>In conclusion, the history of calculators is a journey of innovation. From ancient abacuses to today's digital marvels, these devices have transformed the way we compute. As technology continues to advance, calculators will keep shaping our world, helping us solve problems with precision and efficiency.</p>
+  </article>
+        </div>
+      </div>
+    </div>
+);
+
+export default Home;
